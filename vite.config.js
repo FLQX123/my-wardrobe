bee13fdb21506aabd834c695a05b1e15753730a1
Administrator <admin@wardrobe.local>
2026-06-20 20:44:06 +0800

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
+  base: '/my-wardrobe/',
   plugins: [react(), basicSsl()],
   server: { https: true },
   preview: { https: true }
